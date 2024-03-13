d40c0763839506cbc1538b77fda122bb013ce1be
Marco <marcoleon.rapp@gmail.com>
2024-03-13 19:10:00 +0100

    added support for basic arithmetic operators, added scalarMatrix method, improved dump() method

--- MyMatrixPlayground.playground/Contents.swift
+++ MyMatrixPlayground.playground/Contents.swift
@@ -1,14 +1,12 @@
 import Cocoa
 
 
-
-
 struct Matrix {
     
     private var matrix: [[Double]]
     private let rows: Int
     private let columns: Int
-
+    
     
     /* INITIALIZER */
     
@@ -65,13 +63,19 @@ struct Matrix {
         if value.count > rows || value[0].count > columns { return false }
         matrix = value
         return true
-
+        
     }
     
     /* PRINT */
     
-    func dumpMatrix() {
-        dump(matrix)
+    func dump() {
+        //dump(matrix)
+        for row in 0..<rows {
+            for column in 0..<columns {
+                print(matrix[row][column], terminator: ",\t")
+            }
+            print("\n")
+        }
     }
     
     
@@ -90,7 +94,7 @@ struct Matrix {
     
     mutating func subtractMatrix(_ value: Matrix) -> Bool {
         if value.getRows() != rows || value.getColumns() != columns { return false }
-
+        
         for row in 0..<rows {
             for column in 0..<columns {
                 matrix[row][column] -= value.getElement(row: row, column: column) ?? 0
@@ -99,30 +103,40 @@ struct Matrix {
         return true
     }
     
-    mutating func multiplyMatrix(_ value: Matrix) -> Matrix? {
+    func multiplyMatrix(_ value: Matrix) -> Matrix? {
         // Check if matrices are compatible for multiplication
         guard columns == value.getRows() else {
-               return nil
-           }
-                      // Initialize result matrix with appropriate dimensions
+            return nil
+        }
+        // Initialize result matrix with appropriate dimensions
         var result = Array(repeating: Array(repeating: Double(0), count: value.getColumns()), count: matrix.count)
-           for i in 0..<matrix.count {
-               for j in 0..<value.getColumns() {
-                   var sum = 0.0
-                   for k in 0..<value.getRows() {
-                       sum += matrix[i][k] * (value.getElement(row: k, column: j) ?? 0)
-                   }
-                   result[i][j] = sum
-               }
-           }
-           return Matrix(result)
-       }
+        for i in 0..<matrix.count {
+            for j in 0..<value.getColumns() {
+                var sum = 0.0
+                for k in 0..<value.getRows() {
+                    sum += matrix[i][k] * (value.getElement(row: k, column: j) ?? 0)
+                }
+                result[i][j] = sum
+            }
+        }
+        return Matrix(result)
+    }
+    
+    func scalarMatrix(_ value: Double) -> Matrix {
+        var result = Array(repeating: Array(repeating: Double(0), count: columns), count: rows)
+        for row in 0..<rows {
+            for column in 0..<columns {
+                result[row][column] = matrix[row][column] * value
+            }
+        }
+        return Matrix(result)
+    }
     
     func transposeMatrix() -> Matrix {
         var transposedMatrix = Array(repeating: Array(repeating: Double(0), count: rows), count: columns)
         for row in 0..<rows {
             for column in 0..<columns {
-                transposedMatrix[row][column] = matrix[row][column]
+                transposedMatrix[row][column] = matrix[column][row]
             }
         }
         return Matrix(transposedMatrix)
@@ -131,129 +145,105 @@ struct Matrix {
     func inverseMatrix() -> Matrix? {
         // Matrix must be square for inversion
         guard rows == columns else { return nil }
-            
-            let n = rows
-            var augmentedMatrix = matrix
-            
-            // Augmenting the matrix with identity matrix
-            for i in 0..<n {
-                for j in 0..<n {
-                    if i == j {
-                        augmentedMatrix[i].append(1.0)
-                    } else {
-                        augmentedMatrix[i].append(0.0)
-                    }
+        
+        let n = rows
+        var augmentedMatrix = matrix
+        
+        // Augmenting the matrix with identity matrix
+        for i in 0..<n {
+            for j in 0..<n {
+                if i == j {
+                    augmentedMatrix[i].append(1.0)
+                } else {
+                    augmentedMatrix[i].append(0.0)
                 }
             }
-            // Applying Gaussian elimination to get reduced row-echelon form
-            for i in 0..<n {
-                // Find pivot
-                var pivot = augmentedMatrix[i][i]
-                guard pivot != 0 else {
-                    // If pivot is zero, matrix is not invertible
-                    return nil
-                }
-                
-                // Divide the row by the pivot to make it 1
-                for j in 0..<2 * n {
-                    augmentedMatrix[i][j] /= pivot
-                }
-                
-                // Subtract the pivot row from all other rows to make all elements in the pivot column zero
-                for k in 0..<n {
-                    if k != i {
-                        let multiplier = augmentedMatrix[k][i]
-                        for j in 0..<2 * n {
-                            augmentedMatrix[k][j] -= multiplier * augmentedMatrix[i][j]
-                        }
-                    }
-                }
-            }
-            // Extracting the inverse matrix from the augmented matrix
-            var inverseMatrix = [[Double]]()
-            for i in 0..<n {
-                inverseMatrix.append(Array(augmentedMatrix[i][n..<2 * n]))
-            }
-            return Matrix(inverseMatrix)
         }
+        // Applying Gaussian elimination to get reduced row-echelon form
+        for i in 0..<n {
+            // Find pivot
+            var pivot = augmentedMatrix[i][i]
+            guard pivot != 0 else {
+                // If pivot is zero, matrix is not invertible
+                return nil
+            }
+            
+            // Divide the row by the pivot to make it 1
+            for j in 0..<2 * n {
+                augmentedMatrix[i][j] /= pivot
+            }
+            
+            // Subtract the pivot row from all other rows to make all elements in the pivot column zero
+            for k in 0..<n {
+                if k != i {
+                    let multiplier = augmentedMatrix[k][i]
+                    for j in 0..<2 * n {
+                        augmentedMatrix[k][j] -= multiplier * augmentedMatrix[i][j]
+                    }
+                }
+            }
+        }
+        // Extracting the inverse matrix from the augmented matrix
+        var inverseMatrix = [[Double]]()
+        for i in 0..<n {
+            inverseMatrix.append(Array(augmentedMatrix[i][n..<2 * n]))
+        }
+        return Matrix(inverseMatrix)
+    }
+    
+    
+    /* ARITHMETIC OPERATORS */
+    
+    static func + (left: Matrix, right: Matrix) -> Matrix {
+        var temp = left
+        temp.addMatrix(right)
+        return temp
+    }
+    
+    static func - (left: Matrix, right: Matrix) -> Matrix {
+        var temp = left
+        temp.subtractMatrix(right)
+        return temp
+    }
+    
+    static func * (left: Matrix, scalar: Matrix) -> Matrix? {
+        return left.multiplyMatrix(scalar)
+    }
+    
+    static func * (left: Matrix, scalar: Double) -> Matrix {
+        return left.scalarMatrix(scalar)
+    }
     
 }
 
-/*
-var m = Matrix(rows: 3, columns: 1)
-
-m.dumpMatrix()
 
 
 
-m.setElement(row: 2, column: 0, value: 1.23)
-//if let element = m.getElement(row: 2, column: 0) { print(element) }
 
-
-m.dumpMatrix()
-
-
-let newMatrix: [[Double]] = [[5.0], [6.0], [7.0]]
-
-
-var n = Matrix(newMatrix)
-
-//dump(newMatrix)
-
-m.setMatrix(value: newMatrix)
-
-//if let element = m.getElement(row: 2, column: 0) { print(element) }
-
-m.dumpMatrix()
-n.dumpMatrix()
-*/
-
-
-/*
-var a = Matrix(rows: 3, columns: 1)
-var b = Matrix(rows: 1, columns: 3)
-
-a.setElement(row: 0, column: 0, value: 9)
-a.setElement(row: 1, column: 0, value: 7)
-a.setElement(row: 2, column: 0, value: 5)
-
-b.setElement(row: 0, column: 0, value: 1)
-b.setElement(row: 0, column: 1, value: 2)
-b.setElement(row: 0, column: 2, value: 3)
-
-if var c = a.multiplyMatrix(b) {
-    c.dumpMatrix()
-    let d = Matrix(c.getMatrix())
-    c.subtractMatrix(d)
-    c.dumpMatrix()
-}
- */
-
-
-
-var m = Matrix([
-[1, 3, -2],
-[3, 1, 1],
-[-3, -4, 2]
+var m1 = Matrix([
+    [1, 3, -2],
+    [3, 1, 1],
+    [-3, -4, 2]
 ])
 
-let k = Matrix([
-[2, 3, -2],
-[3, 4, 1],
-[-3, -5, 6]
+let m2 = Matrix([
+    [2, 3, -2],
+    [3, 4, 1],
+    [-3, -5, 6]
 ])
 
-m.dumpMatrix()
-
-m.subtractMatrix(k)
-m.addMatrix(k)
 
 
-if let n = m.inverseMatrix()  {
-    
-    n.dumpMatrix()
+if let newMatrix = m1 * m2{
+    //newMatrix.dump()
+    print()
 }
 
+let m3 = m2.transposeMatrix()
+let m4 = m2.inverseMatrix()
 
-
-
+m2.dump()
+print()
+m3.dump()
+print()
+m4?.dump()
